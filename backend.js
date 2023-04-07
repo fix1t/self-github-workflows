57ded32eb08a244fef9c5371bac33448cf161c80
Fix1t <gbiel1117@gmail.com>
2023-04-07 14:50:00 +0200

    done push all idc

---
+++ backend.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrbZgqDXun4qv9j1Xqsevw-KOxPNFQ26U",
+  authDomain: "hw-test-5b55c.firebaseapp.com",
+  projectId: "hw-test-5b55c",
+  storageBucket: "hw-test-5b55c.appspot.com",
+  messagingSenderId: "186861344499",
+  appId: "1:186861344499:web:9e7aa251b2bf4fc1658986"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
